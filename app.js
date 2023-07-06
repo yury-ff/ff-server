0bacace2f705c8eb94839dd7dc0360b42a6fe1ea
yury-ff <yury.r@forkedfinance.xyz>
2023-07-06 12:41:59 +0100

    second commit

--- app.js
+++ app.js
@@ -43,6 +43,17 @@ app.use(cors());
 app.use(xss());
 app.use(mongoSanitize());
 app.use(cors());
+app.use(function (req, res, next) {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://ff-front-end.onrender.com"
+  ); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
