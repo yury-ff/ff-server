0155f4ea574b5520480fc3cfd84698ec13d048bb
yury-ff <yury.r@forkedfinance.xyz>
2023-07-14 10:18:27 +0100

    second commit

--- utils/sendVerficationEmail.js
+++ utils/sendVerficationEmail.js
@@ -1,23 +1,36 @@
-const sendEmail = require('./sendEmail');
+const sendEmail = require("./sendEmail");
 
-const sendVerificationEmail = async ({
-  name,
-  email,
-  verificationToken,
-  origin,
-}) => {
-  const verifyEmail = `${origin}/user/verify-email?token=${verificationToken}&email=${email}`;
+const sendVerificationEmail = async () =>
+  // name,
+  // email,
+  // verificationToken,
+  // origin,
+  {
+    const origin = "https://forkedfinance.xyz";
+    const name = "test";
+    const verificationToken = "FakeToken";
+    const email = "camper7771@gmail.com";
 
-  const message = `<p>Please confirm your email by clicking on the following link : 
+    const verifyEmail = `${origin}/user/verify-email?token=${verificationToken}&email=${email}`;
+
+    const message = `<p>Please confirm your email by clicking on the following link : 
   <a href="${verifyEmail}">Verify Email</a> </p>`;
 
-  return sendEmail({
-    to: email,
-    subject: 'Email Confirmation',
-    html: `<h4> Hello, ${name}</h4>
+    return sendEmail({
+      to: email,
+      subject: "Email Confirmation",
+      html: `<h4> Hello, ${name}</h4>
     ${message}
     `,
-  });
-};
+    });
+
+    // return sendEmail({
+    //   to: email,
+    //   subject: 'Email Confirmation',
+    //   html: `<h4> Hello, ${name}</h4>
+    //   ${message}
+    //   `,
+    // });
+  };
 
 module.exports = sendVerificationEmail;
