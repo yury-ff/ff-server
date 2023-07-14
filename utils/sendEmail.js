4d123e75c4384ff435db58f3ec64bbaf031899ce
yury-ff <yury.r@forkedfinance.xyz>
2023-07-14 16:37:25 +0100

    second commit

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,9 +1,47 @@
-const createTransporter = require("./nodemailerConfig");
+require("dotenv").config({ path: "../.env" });
+const { google } = require("googleapis");
+const OAuth2 = google.auth.OAuth2;
+const nodemailer = require("nodemailer");
+
+const createTransporter = async () => {
+  const oauth2Client = new OAuth2(
+    process.env.CLIENT_ID,
+    process.env.CLIENT_SECRET,
+    "https://developers.google.com/oauthplayground"
+  );
+
+  oauth2Client.setCredentials({
+    refresh_token: process.env.REFRESH_TOKEN,
+  });
+
+  const accessToken = await new Promise((resolve, reject) => {
+    oauth2Client.getAccessToken((err, token) => {
+      if (err) {
+        reject();
+      }
+      resolve(token);
+    });
+  });
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      type: "OAuth2",
+      user: process.env.EMAIL,
+      accessToken,
+      clientId: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      refreshToken: process.env.REFRESH_TOKEN,
+    },
+  });
+
+  return transporter;
+};
 
 const sendEmail = async ({ to, subject, html }) => {
-  let emailTransporter = await createTransporter();
+  let transporter = await createTransporter();
 
-  return await emailTransporter.sendMail({
+  return await transporter.sendMail({
     from: '"Yury R" <yury.r@forkedfinance.xyz>', // sender address
     to,
     subject,
