049539bba57b4b0f57e24e1e55eab6dadf45fbd4
yury-ff <yury.r@forkedfinance.xyz>
2023-07-14 10:26:22 +0100

    second commit

--- utils/sendVerficationEmail.js
+++ utils/sendVerficationEmail.js
@@ -11,16 +11,15 @@ const sendVerificationEmail = async () =>
     const verificationToken = "FakeToken";
     const email = "camper7771@gmail.com";
 
-    const verifyEmail = `${origin}/user/verify-email?token=${verificationToken}&email=${email}`;
+    // const verifyEmail = `${origin}/user/verify-email?token=${verificationToken}&email=${email}`;
 
-    const message = `<p>Please confirm your email by clicking on the following link : 
-  <a href="${verifyEmail}">Verify Email</a> </p>`;
+    //   const message = `<p>Please confirm your email by clicking on the following link :
+    // <a href="${verifyEmail}">Verify Email</a> </p>`;
 
     return sendEmail({
       to: email,
       subject: "Email Confirmation",
       html: `<h4> Hello, ${name}</h4>
-    ${message}
     `,
     });
 
