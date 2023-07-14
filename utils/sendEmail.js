2acaa0103de6224b6d4afc397b2851d6fbd85a13
yury-ff <yury.r@forkedfinance.xyz>
2023-07-14 17:01:27 +0100

    second commit

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -3,7 +3,42 @@ const { google } = require("googleapis");
 const OAuth2 = google.auth.OAuth2;
 const nodemailer = require("nodemailer");
 
-const createTransporter = async () => {
+// const createTransporter = async () => {
+//   const oauth2Client = new OAuth2(
+//     process.env.CLIENT_ID,
+//     process.env.CLIENT_SECRET,
+//     "https://developers.google.com/oauthplayground"
+//   );
+
+//   oauth2Client.setCredentials({
+//     refresh_token: process.env.REFRESH_TOKEN,
+//   });
+
+//   const accessToken = await new Promise((resolve, reject) => {
+//     oauth2Client.getAccessToken((err, token) => {
+//       if (err) {
+//         reject();
+//       }
+//       resolve(token);
+//     });
+//   });
+
+//   const transporter = nodemailer.createTransport({
+//     service: "gmail",
+//     auth: {
+//       type: "OAuth2",
+//       user: process.env.EMAIL,
+//       accessToken,
+//       clientId: process.env.CLIENT_ID,
+//       clientSecret: process.env.CLIENT_SECRET,
+//       refreshToken: process.env.REFRESH_TOKEN,
+//     },
+//   });
+
+//   return transporter;
+// };
+
+const sendEmail = async ({ to, subject, html }) => {
   const oauth2Client = new OAuth2(
     process.env.CLIENT_ID,
     process.env.CLIENT_SECRET,
@@ -35,13 +70,7 @@ const createTransporter = async () => {
     },
   });
 
-  return transporter;
-};
-
-const sendEmail = async ({ to, subject, html }) => {
-  let transporter = await createTransporter();
-
-  return await transporter.sendMail({
+  return transporter.sendMail({
     from: '"Yury R" <yury.r@forkedfinance.xyz>', // sender address
     to,
     subject,
